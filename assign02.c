fb316a7fd790504794da54e1d6057cc7f28a4310
Patrick Neil Farmer <farmerp@tcd.ie>
2023-04-08 21:45:13 +0100

    Ability to select level should now work but needs arm to test

--- assign02.c
+++ assign02.c
@@ -1,70 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include "assign02Arrays.h"
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
+#include "hardware/pio.h"
 
-// Declare the main assembly code entry point.
-void main_asm();
 void asm_gpio_init(uint pin);
 void asm_gpio_set_dir(uint pin, bool out);
 bool asm_gpio_get(uint pin);
 void asm_gpio_put(uint pin, bool value);
 void asm_gpio_set_irq(uint pin);
 void playGame();
-
+void main_asm();
 
 // Main entry point of the application
 int main() {
     setUpArrays();
     stdio_init_all();              // Initialise all basic IO
-    printf("Assignment #2...\n");  // Basic print to console
     main_asm();                    // Jump into the ASM code
     playGame();
     return 0;                      // Application return code
-}
-
-void playGame(){
-    printf("\033[1;34m");
-    put_pixel(urgb_u32(0x00, 0x00, 0x00));
-    printf("||===================================================||\n");
-    printf("||  _       _   _______   _______   _______   _____  ||\n");
-    printf("|| |:|     |:| |:::::::| |:::::::| |:::::::| |:::::| ||\n");
-    printf("|| |::|   |::| |:|   |:| |:|   |:| |:|       |:|     ||\n");
-    printf("|| |:|:| |:|:| |:|   |:| |:|___|:| |:|_____  |:|___  ||\n");
-    printf("|| |:||:|:||:| |:|   |:| |::::::|  |:::::::| |:::::| ||\n");
-    printf("|| |:| |:| |:| |:|   |:| |:||:|          |:| |:|     ||\n");
-    printf("|| |:|     |:| |:|___|:| |:| |:|    _____|:| |:|___  ||\n");
-    printf("|| |:|     |:| |:::::::| |:|  |:|  |:::::::| |:::::| ||\n");
-    printf("||      _________     ___     _       _   _____      ||\n");
-    printf("||     |:::::::::|   |:::|   |:|     |:| |:::::|     ||\n");
-    printf("||     |:|     |:|  |:| |:|  |::|   |::| |:|         ||\n");
-    printf("||     |:|         |:|   |:| |:|:| |:|:| |:|___      ||\n");
-    printf("||     |:|    ___  |:|___|:| |:||:|:||:| |:::::|     ||\n");
-    printf("||     |:|   |:::| |:::::::| |:| |:| |:| |:|         ||\n");
-    printf("||     |:|_____|:| |:|   |:| |:|     |:| |:|___      ||\n");
-    printf("||     |:::::::::| |:|   |:| |:|     |:| |:::::|     ||\n");
-    printf("||===================================================||\n");
-    printf("||                   [HOW TO PLAY]                   ||\n");
-    printf("||===================================================||\n");
-    printf("||       Characters Will Appear On Your Screen       ||\n");
-    printf("||    You Must Enter The Corresponding Morse Code    ||\n");
-    printf("||      Press The GP21 Button Quickly For A Dot      ||\n");
-    printf("||      Press The GP21 Button Longer For A Dash      ||\n");
-    printf("||          If Correct You Will Gain A Life          ||\n");
-    printf("||               (Maximum Of 3 lives)                ||\n");
-    printf("||         If Incorrect You Will Lose A Life         ||\n");
-    printf("||      When You Lose All Lives, it's Game Over      ||\n");
-    printf("||    After 5 Correct Sequences You Win The Level    ||\n");
-    printf("||===================================================||\n");
-    printf("\033[0;39m");
-
-    printf("||===================================================||\n");
-    printf("||                   Select Level:                   ||\n");
-    printf("||===================================================||\n");
-    printf("||             Enter '.----' for level 1             ||\n");
-    printf("||             Enter '..---' for level 2             ||\n");
-    printf("||             Enter '...--' for level 3             ||\n");
-    printf("||             Enter '....-' for level 4             ||\n");
-    printf("||===================================================||\n");
 }
 
 // Initialise a GPIO pin – see SDK for detail on gpio_init()
@@ -91,3 +48,73 @@ void asm_gpio_put(uint pin, bool value) {
 void asm_gpio_set_irq(uint pin) {
     gpio_set_irq_enabled(pin, GPIO_IRQ_EDGE_FALL, true);
 }
+
+void playGame(){
+    printf("\033[1;34m");
+	put_pixel(urgb_u32(0x00, 0x00, 0x00));
+	printf("||===================================================||\n");
+	printf("||  _       _   _______   _______   _______   _____  ||\n");
+	printf("|| |:|     |:| |:::::::| |:::::::| |:::::::| |:::::| ||\n");
+	printf("|| |::|   |::| |:|   |:| |:|   |:| |:|       |:|     ||\n");
+	printf("|| |:|:| |:|:| |:|   |:| |:|__|:| |:|____  |:|___  ||\n");
+	printf("|| |:||:|:||:| |:|   |:| |::::::|  |:::::::| |:::::| ||\n");
+	printf("|| |:| |:| |:| |:|   |:| |:||:|          |:| |:|     ||\n");
+	printf("|| |:|     |:| |:|__|:| |:| |:|    _____|:| |:|__  ||\n");
+	printf("|| |:|     |:| |:::::::| |:|  |:|  |:::::::| |:::::| ||\n");
+	printf("||      _________     ___     _       _   _____      ||\n");
+	printf("||     |:::::::::|   |:::|   |:|     |:| |:::::|     ||\n");
+	printf("||     |:|     |:|  |:| |:|  |::|   |::| |:|         ||\n");
+	printf("||     |:|         |:|   |:| |:|:| |:|:| |:|___      ||\n");
+	printf("||     |:|    ___  |:|___|:| |:||:|:||:| |:::::|     ||\n");
+	printf("||     |:|   |:::| |:::::::| |:| |:| |:| |:|         ||\n");
+	printf("||     |:|____|:| |:|   |:| |:|     |:| |:|__      ||\n");
+	printf("||     |:::::::::| |:|   |:| |:|     |:| |:::::|     ||\n");
+	printf("||===================================================||\n");
+	printf("||                  [HOW TO PLAY]                   ||\n");
+	printf("||===================================================||\n");
+	printf("||       Characters Will Appear On Your Screen       ||\n");
+	printf("||    You Must Enter The Corresponding Morse Code    ||\n");
+	printf("||      Press The GP21 Button Quickly For A Dot      ||\n");
+	printf("||      Press The GP21 Button Longer For A Dash      ||\n");
+	printf("||          If Correct You Will Gain A Life          ||\n");
+	printf("||               (Maximum Of 3 lives)                ||\n");
+	printf("||         If Incorrect You Will Lose A Life         ||\n");
+	printf("||      When You Lose All Lives, it's Game Over      ||\n");
+	printf("||    After 5 Correct Sequences You Win The Level    ||\n");
+	printf("||===================================================||\n");
+	printf("\033[0;39m");
+
+    printf("||===================================================||\n");
+	printf("||                   Select Level:                   ||\n");
+	printf("||===================================================||\n");
+	printf("||             Enter '.----' for level 1             ||\n");
+	printf("||             Enter '..---' for level 2             ||\n");
+	printf("||             Enter '...--' for level 3             ||\n");
+	printf("||             Enter '....-' for level 4             ||\n");
+	printf("||===================================================||\n");
+
+    input();
+
+    // Obtain array from interrupts and compare with morse array
+    char *interruptArray = grabArray();
+
+    // Determine level based on input string
+    for (int levelIndex = 1; levelIndex <= 4; levelIndex++) {
+        char tempArray[6];
+
+        // Assign values from input to the temporary array
+        for (int i = 0; i < 6; i++) {
+            tempArray[i] = *(interruptArray + (i * sizeof(int)));
+        }
+
+        tempArray[5] = '\0'; // Ensure the temporary array is null-terminated
+
+        // Compare the temporary array with the morse strings
+        if (strcmp(tempArray, morseStruct[levelIndex].morseCode) == 0) {
+            level = levelIndex;
+            break;
+        }
+    }
+}
+
+
